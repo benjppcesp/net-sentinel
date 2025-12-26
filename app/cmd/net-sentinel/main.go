e65b0245682c929b3ef4928d6172cb7259f2bfe4
Pasante <Pasante@cespal.local>
2025-12-26 10:29:46 -0300

    nueva rama y cambios en codigo

--- app/cmd/net-sentinel/main.go
+++ app/cmd/net-sentinel/main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	targetURL      string
-	checkInterval  time.Duration
-	requestTimeout time.Duration
-)
+// Definicion de Objeto (POO)
+type Sentinel struct {
+	TargetURL      string        `json:"target_url"`
+	CheckInterval  time.Duration `json:"check_interval"`
+	RequestTimeout time.Duration `json:"request_timeout"`
+	Status         string        `json:"status"`
+	mu             sync.RWMutex  // Protege el objeto de accesos simultáneos
+}
+
+func (s *Sentinel) SetStatus(newStatus string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Status = newStatus
+}
 
 var (
 	httpDuration = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:    "net_sentinel_http_duration_seconds",
 		Help:    "Tiempo de respuesta de la petición HTTP",
-		Buckets: prometheus.DefBuckets,
+		Buckets: []float64{0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0},
 	})
 
 	httpSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "net_sentinel_http_success",
 		Help: "Estado de la conexión: 1 si fue exitosa, 0 si falló",
+	})
+
+	apiRequest = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "net_sentinel_api_calls_total",
+		Help: "Total de consultas a la API de Control",
 	})
 )
 
@@ -33,14 +50,70 @@ func init() {
 	prometheus.MustRegister(httpDuration)
 	prometheus.MustRegister(httpSuccess)
 
-	loadConfig()
 }
 
-func loadConfig() {
-	targetURL = getENV("TARGET_URL", "https://www.bing.com")
-	checkInterval = getDurationEnv("CHECK_INTERVAL", 5*time.Second)
-	requestTimeout = getDurationEnv("REQUEST_TIMEOUT", 5*time.Second)
+func (s *Sentinel) RunProbe() {
+	go func() {
+		for {
+			s.mu.RLock()
+			url := s.TargetURL
+			timeout := s.RequestTimeout
+			interval := s.CheckInterval
+			s.mu.RUnlock()
 
+			client := &http.Client{Timeout: timeout}
+			start := time.Now()
+
+			resp, err := client.Get(url)
+			duration := time.Since(start).Seconds()
+
+			if err != nil {
+				fmt.Printf("❌ Error: %v\n", err)
+				httpSuccess.Set(0)
+				s.mu.Lock()
+				s.Status = "Error de Conexion"
+				s.mu.Unlock()
+			} else {
+				httpSuccess.Set(1)
+				httpDuration.Observe(duration)
+				resp.Body.Close()
+				s.mu.Lock()
+				s.Status = "Online"
+				s.mu.Unlock()
+			}
+			time.Sleep(interval)
+
+		}
+	}()
+}
+
+func main() {
+
+	sonda := &Sentinel{
+		TargetURL:      getENV("TARGET_URL", "https://www.bing.com"),
+		CheckInterval:  5 * time.Second,
+		RequestTimeout: 5 * time.Second,
+		Status:         "Iniciando",
+	}
+	sonda.RunProbe()
+
+	go func() {
+		fmt.Println("📊 Métricas en :2112/metrics")
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":2112", nil)
+	}()
+	// Endopoint de API (Puerto 8080)
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		apiRequest.Inc()
+		w.Header().Set("Content-type", "application/json")
+		sonda.mu.RLock()
+		json.NewEncoder(w).Encode(sonda)
+		sonda.mu.RUnlock()
+
+	})
+
+	fmt.Println("🚀 API de Control en :8080/status")
+	http.ListenAndServe(":8080", nil)
 }
 
 func getENV(key, defaultValue string) string {
@@ -48,53 +121,4 @@ func getENV(key, defaultValue string) string {
 		return value
 	}
 	return defaultValue
-}
-
-func getDurationEnv(key string, defaultValue time.Duration) time.Duration {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-
-	duration, err := time.ParseDuration(value)
-	if err != nil {
-		panic(fmt.Sprintf("variable %s invalida: %s"))
-	}
-	return duration
-}
-
-func probeNetwork() {
-	go func() {
-		client := &http.Client{
-			Timeout: requestTimeout,
-		}
-
-		for {
-			start := time.Now()
-
-			resp, err := client.Get(targetURL)
-			duration := time.Since(start).Seconds()
-
-			if err != nil {
-				fmt.Printf("❌ Error conectando a %s: %v\n", targetURL, err)
-				httpSuccess.Set(0)
-			} else {
-				fmt.Printf("✅ Conexión exitosa a %s en %.4f segundos\n", targetURL, duration)
-				httpSuccess.Set(1)
-				httpDuration.Observe(duration)
-				resp.Body.Close()
-			}
-
-			time.Sleep(checkInterval)
-		}
-	}()
-}
-
-func main() {
-	fmt.Println("📡 Net-Sentinel iniciado. Monitoreando:", targetURL)
-	probeNetwork()
-
-	fmt.Println("🎧 Servidor de métricas escuchando en puerto :2112")
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
 }
